7294d7f0f3055e488b1ff0ee84e6f99b624227ac
IROHA-123 <192299303+IROHA-123@users.noreply.github.com>
2025-09-10 10:47:20 +0900

    chore: disable user self-signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
   # admin かどうか、true or false　を返す
   def admin?
@@ -10,6 +10,6 @@ class User < ApplicationRecord
 
   has_many :shift_requests
   has_many :shift_assignments
-  
+
 
 end
